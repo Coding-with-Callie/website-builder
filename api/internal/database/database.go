446e63fac338c8609ce38499f42066bcd55b229c
Callie Stoscup <calliestoscup@gmail.com>
2025-01-26 09:03:46 -0500

    Add logging to database set up

--- api/internal/database/database.go
+++ api/internal/database/database.go
@@ -5,6 +5,8 @@ import (
 	"database/sql"
 	"fmt"
 
+	"github.com/rs/zerolog/log"
+
 	_ "github.com/lib/pq"
 )
 
@@ -19,39 +21,43 @@ func Connect() {
 	// Connect to defailt postgres database
 	db, err := sql.Open("postgres", fmt.Sprintf("user=%s password=%s dbname=postgres sslmode=disable", user, password))
 	if err != nil {
-		panic(err)
+		log.Fatal().Err(err).Msg("Failed to connect to default postgres database")
 	}
 
 	// Create a website-builder database if it doesn't exist
 	_, err = db.Exec("CREATE DATABASE website_builder")
-	if err != nil && err.Error() != "pq: database \"website_builder\" already exists" {
-		panic(err)
+	if err != nil {
+		if err.Error() == "pq: database \"website_builder\" already exists" {
+			log.Info().Msg("Website builder database already exists")
+		} else {
+			log.Fatal().Err(err).Msg("Failed to create website builder database")
+		}
 	}
 
 	// Close the connection to the default database
 	err = db.Close()
 	if err != nil {
-		fmt.Println(err.Error())
+		log.Fatal().Err(err).Msg("Failed to close connection to default database")
 	}
 
 	// Connect to the website-builder database
 	db, err = sql.Open("postgres", fmt.Sprintf("user=%s password=%s dbname=website_builder sslmode=disable", user, password))
 	if err != nil {
-		panic(err)
+		log.Fatal().Err(err).Msg("Failed to connect to website builder database")
 	}
 
 	// Check if the connection is successful
 	err = db.Ping()
 	if err != nil {
-		panic(err)
+		log.Fatal().Err(err).Msg("Failed to ping website builder database")
 	}
 
-	fmt.Println("Connected to database")
+	log.Info().Msg("Connected to website builder database")
 
 	// Create a users table if it doesn't exist
 	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, first_name VARCHAR(50), last_name VARCHAR(50), email VARCHAR(50), username VARCHAR(50), password VARCHAR(255), role VARCHAR(50), photo VARCHAR(255))")
 	if err != nil {
-		panic(err)
+		log.Fatal().Err(err).Msg("Failed to create users table")
 	}
 
 	DB = db
