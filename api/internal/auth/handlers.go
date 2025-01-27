9f913be6037d9b2a3f6b73f6ce7e5507f2e07200
Callie Stoscup <calliestoscup@gmail.com>
2025-01-26 20:33:10 -0500

    Refactored a little

--- api/internal/auth/handlers.go
+++ api/internal/auth/handlers.go
@@ -13,31 +13,13 @@ type LoginRequestBody struct {
 }
 
 func RegisterAuthHandlers(router *gin.Engine, authService AuthService, logger zerolog.Logger) {
-	router.POST("/login", func(c *gin.Context) {
-		Login(c, authService)
-	})
+	router.POST("/login", func(c *gin.Context) { Login(c, authService) })
 
 	// Protected routes
 	authorized := router.Group("/auth/")
 	authorized.Use(AuthMiddleware(logger))
 	{
-		authorized.GET("/user-details", func(c *gin.Context) {
-			username := c.MustGet("username").(string)
-
-			// Get user details from the database
-			userDetails, err := authService.GetUserDetails(username)
-			if err != nil {
-				c.JSON(http.StatusBadRequest, gin.H{
-					"message": "Invalid request",
-				})
-				return
-			}
-
-			// Return user details
-			c.JSON(http.StatusOK, gin.H{
-				"user": userDetails,
-			})
-		})
+		authorized.GET("/user-details", func(c *gin.Context) { GetUserDetails(c, authService) })
 	}
 }
 
@@ -64,5 +46,23 @@ func Login(c *gin.Context, authService AuthService) {
 	// Return the access token
 	c.JSON(http.StatusOK, gin.H{
 		"access_token": access_token,
+	})
+}
+
+func GetUserDetails(c *gin.Context, authService AuthService) {
+	username := c.MustGet("username").(string)
+
+	// Get user details from the database
+	userDetails, err := authService.GetUserDetails(username)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid request",
+		})
+		return
+	}
+
+	// Return user details
+	c.JSON(http.StatusOK, gin.H{
+		"user": userDetails,
 	})
 }
