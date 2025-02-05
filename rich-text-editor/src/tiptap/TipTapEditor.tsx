88ae2fee47439736769032a2bae94f69c2751456
Kris10 <kmrichie611@gmail.com>
2025-02-05 12:55:46 -0600

    created a simple form to test the TipTap editor with the form

--- rich-text-editor/src/tiptap/TipTapEditor.tsx
+++ rich-text-editor/src/tiptap/TipTapEditor.tsx
@@ -1,11 +1,11 @@
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import TipTapToolbar from "./TipTapToolbar";
 import Underline from "@tiptap/extension-underline";
 import Link from "@tiptap/extension-link";
 
-
+//tiptap extensions like nodes and marks. The starter kit contains the majority of the node and mark extensions, but you will need to manually add some like Underline and Link.
 const extensions = [
     StarterKit, 
     Underline, 
@@ -16,13 +16,16 @@ const extensions = [
     })
 ];
 
-const content = '<p>Hello, this is TipTap!</p> <p>Hi, Again</p>'
+//this is the content that will be styled using the editor
+// const content =  <p>Hi, Again</p>'
 
 const TipTapEditor = () => {
     const editor = useEditor({
         extensions,
-        content,
-    })
+        content: '<p>Hello, this is TipTap!</p>'
+    });
+
+    const [content, setContent] = useState("");
 
     // if editor changes, the effect will run
     // cleanup function will run when component is unmounted or reset. destroy() helps prevent memory leaks or lingering event listeners
@@ -32,15 +35,40 @@ const TipTapEditor = () => {
         }
     }, [editor]);
 
+    //handle formm submission
+    const handleFormSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if (editor) {
+            setContent(editor.getHTML()); //save the content
+            console.log("SUBMITTED CONTENT:", editor.getHTML());
+        }
+        
+    }
+
+    // Prevent rendering before initialization
     if (!editor) {
-        return <p>Loading editor...</p>; // Prevent rendering before initialization
+        return <p>Loading editor...</p>; 
     }
   return (
     <>
-        <div>
+        <form onSubmit={handleFormSubmit}>
             <h2>TipTap Editor</h2>
             <TipTapToolbar editor={editor}/>
             <EditorContent editor={editor}/>
+            <button type='submit'>Submit</button>
+
+            {/* Displaying stored content for verification */}
+            {/* {content && (
+                <div>
+                    <h3>Preview: </h3>
+                    <div dangerouslySetInnerHTML={{__html: content}}></div>
+                </div>
+            )} */}
+
+        </form>
+        <div>
+            
         </div>
     
     </>
